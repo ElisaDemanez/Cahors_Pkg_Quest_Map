acc2cc2b019301dfbdb855c80037f7a38186d2aa
lisaDemanez <elisa.demanez@gmail.com>
2018-11-26 09:25:23 +0100

    mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// const MongoClient    = require('mongodb').MongoClient;
 // const bodyParser     = require('body-parser');
 var express = require('express');
 var app = express();
+const MongoClient    = require('mongodb').MongoClient;
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var coordinates = require('./pokestops_cahors.json');
@@ -24,3 +25,20 @@ app.use(express.static('public'));
   http.listen(port, function(){
     console.log('listening on *:8000');
   });
+
+
+  // Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  if (err) throw err;
+
+  const db = client.db(dbName);
+  console.log("Connected successfully to server");
+
+  client.close();
+});
